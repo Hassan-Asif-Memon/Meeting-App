6f474d2453edb829b3022ad24053c71b7a0e34de
Hassan-Asif-Memon <hm0078275@gmail.com>
2026-07-18 22:05:59 +0500

    New Material

--- react/src/components/Login.jsx
+++ react/src/components/Login.jsx
@@ -6,6 +6,10 @@ function Login(){
         <div className="left">
             <div className="logo">
                 <img src={meetmind} alt="MeetMind" />
+                <div className="text">
+                    <h2>MeetMind AI</h2>
+                    <p>AI Meeting Notes & Task Manager</p>
+                </div>
             </div>
         </div>
     </div>
