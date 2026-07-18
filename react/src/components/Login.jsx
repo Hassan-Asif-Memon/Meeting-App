63e9cc1c2ca32b43f34a53c01850646668fb991b
Hassan-Asif-Memon <hm0078275@gmail.com>
2026-07-18 19:04:23 +0500

    logo added

--- react/src/components/Login.jsx
+++ react/src/components/Login.jsx
@@ -1,7 +1,14 @@
+import meetmind from "../assets/meetmind.png"
 function Login(){
 
     return(<>
-    
+    <div className="container">
+        <div className="left">
+            <div className="logo">
+                <img src={meetmind} alt="MeetMind" />
+            </div>
+        </div>
+    </div>
     </>)
 }
 export default Login;
