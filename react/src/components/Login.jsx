aa21c7db0ff06881051b925f0cf7a6d38fa4f4aa
Hassan-Asif-Memon <hm0078275@gmail.com>
2026-07-19 19:21:13 +0500

    Work is in Progress

--- react/src/components/Login.jsx
+++ react/src/components/Login.jsx
@@ -14,11 +14,35 @@ function Login(){
                 </div>
             </div>
             <img className="meeting" src={meeting} alt="Meeting" />
+            <div className="paras">
+                <p>Let AI handle your meeting notes</p>
+                <p>so you can focus on what matters.</p>
+            </div>
         </div>
 
 
         <div className="right">
-            
+            <div className="login-form">
+                <h1 className="title">Welcome Back</h1>
+                <h4 className="subtitle">Sign in to continue</h4>
+                <div className="form">
+                    <label htmlFor="email or username">Email or Username</label>
+                    <input type="text" id="email-user" placeholder="Enter your email or username" />
+                    <label htmlFor="password">Password</label>
+                    <div className="password-box">
+                    <input type="password" name="password" id="password" placeholder="Enter your password" />
+                    <span>👁️</span>
+                    <input type="checkbox" />
+                    <p>Remember me</p>
+                    <a href="#">Forgot password?</a>
+                    <button>Sign In</button>
+                    <div className="register">
+                        <p>Don't have an account? <a href="#">Register now</a></p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
     </>)
